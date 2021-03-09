68db2b476f23440dafd4c68d4d19fc7b9eb5da46
dsternalski <d.sternalski@yahoo.co.uk>
2021-03-09 17:22:24 +0000

    Making it clickable
    Updated the following:
    - Changed the List.map into a curried function, meaning it is partially applied.
    - Added the onclick function to the image and imported the Browser.
    - The main so that it is no longer just a static page.
    - Reformatted the code so that it is neater and easier to read.

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,23 +1,20 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 urlPrefix = 
     "http://elm-in-action.com/"
 
 view model = 
-    div [class "content"]
-        [h1 [] [text "Photo Groove"]
-        , div [id "thumbnails"] 
-            (List.map 
-                -- \photo is the name of the annoymous function and uses that SAME name at the end of the function call. Think of photo (at the begining and end) in the same way as a 'value', e.g. var a = 0 in a for loop.
-                (\photo -> viewThumbnail model.selectedUrl photo)
-                --  The model in this instance is refering to the initalModel as this is what is called in main.
-                model.photos 
-            )
+    div [ class "content" ]
+        [h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnails" ] 
+            ( List.map ( viewThumbnail model.selectedUrl ) model.photos )
         , img 
-            [class "large"
+            [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
             []
@@ -25,19 +22,30 @@ view model =
 
 viewThumbnail selectedUrl thumb = 
     img 
-        [src (urlPrefix ++ thumb.url)
-        , classList [("selected", selectedUrl == thumb.url)]
+        [ src ( urlPrefix ++ thumb.url )
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
         ] 
         []
 
 initalModel = 
     {photos = 
-        [{url = "1.jpeg"}
-        ,{url = "2.jpeg"}
-        ,{url = "3.jpeg"}
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
     }
 
+update msg model = 
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data }
+    else
+        model
+
 main = 
-    view initalModel
+    Browser.sandbox
+        { init = initalModel
+        , view = view
+        , update = update
+        }
