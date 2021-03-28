3ac4ad8a00462c7eabc035af686346fe8dae18f0
dsternalski <d.sternalski@yahoo.co.uk>
2021-03-28 16:40:24 +0100

    Added the slideHueSetHue test function

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -3,7 +3,7 @@ module PhotoGrooveTests exposing (..)
 import Expect exposing (Expectation)
 import Json.Decode as Decode exposing (decodeValue)
 import Json.Encode as Encode
-import PhotoGroove
+import PhotoGroove exposing (Model, Msg(..), Photo, initalModel, update)
 import Test exposing (..)
 import Fuzz exposing (Fuzzer, int, list, string)
 
@@ -19,4 +19,15 @@ decoderTest =
                 |> decodeValue PhotoGroove.photoDecoder
                 |> Result.map .title
                 |> Expect.equal ( Ok "(untitled)" )
+
+
+slideHueSetHue : Test
+slideHueSetHue = 
+    fuzz int "SlidHue sets the hue" <|
+        \amount -> 
+            initalModel
+                |> update ( SlidHue amount )
+                |> Tuple.first
+                |> .hue
+                |> Expect.equal amount
         
