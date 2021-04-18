4e9469c0d4cae0893a9a2cca5194ee5047d83b10
dsternalski <d.sternalski@yahoo.co.uk>
2021-04-18 18:18:44 +0100

    Updating the page further with a header and footer.

--- src/Main.elm
+++ src/Main.elm
@@ -1,22 +1,65 @@
-module Main exposing (Main)
+module Main exposing (main)
 
 import Browser exposing (Document)
 import Html exposing (Html, a, footer, h1, li, nav, text, ul)
-import Html.Attribute exposing (classList, href)
+import Html.Attributes exposing (classList, href)
 
 type alias Model =
-    {}
+    { page : Page }
 
 
-view : Model -> Document Msg
-view model = 
-    { title = "Photo Groove, SPA Style" 
-    , body = [ text "This isn't even my final form!" ] 
-    }
+type Page
+     = Gallery
+     | Folders
+     | NotFound
 
 
 type Msg
      = NothingYet
+
+
+view : Model -> Document Msg
+view model = 
+    let
+        content = 
+            text "This isn't even my final form!"
+
+    in
+    { title = "Photo Groove, SPA Style" 
+    , body = 
+        [ viewHeader model.page 
+        , content
+        , viewFooter
+        ] 
+    }
+
+
+viewHeader : Page -> Html Msg
+viewHeader page = 
+    let
+        logo = 
+            h1 [] [ text "Photo Groove" ]
+
+        links = 
+            ul [] 
+                [ navLink Folders { url = "/", caption = "Folders" }
+                , navLink Gallery { url = "/galler", caption = "Gallery" }
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } = 
+            li [ classList [ ( "active", page == targetPage ) ] ]
+                [a [ href url ] [ text caption ] ]
+
+    in
+    nav [] [ logo, links ]
+
+
+viewFooter : Html msg
+viewFooter = 
+    footer [] [ text "One is never alone with a rubber duck. - Douglas Adams" 
+    ]
+
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -31,8 +74,8 @@ subscriptions model =
 
 main : Program () Model Msg
 main = 
-    Browser.Document
-        { init = \_ -> ( {}, Cmd.none )
+    Browser.document
+        { init = \_ -> ( { page = Gallery }, Cmd.none )
         , subscriptions = subscriptions
         , update = update
         , view = view
