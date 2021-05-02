7f9ef2f53fa9b40d81dace90baee33883229a266
dsternalski <d.sternalski@yahoo.co.uk>
2021-05-02 18:25:53 +0100

    Added the code to tie the pages together.

--- src/PhotoFolders.elm
+++ src/PhotoFolders.elm
@@ -40,9 +40,9 @@ initialModel =
     }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( initialModel
+init : Maybe String -> ( Model, Cmd Msg )
+init selectedFilename =
+    ( { initialModel | selectedPhotoUrl = selectedFilename }
     , Http.get
         { url = "http://elm-in-action.com/folders/list"
         , expect = Http.expectJson GotInitialModel modelDecoder
